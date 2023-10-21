c28eafe8902f3cc751afb337ff7df9004707c06a
Joe Chang <131396136+josephachang02@users.noreply.github.com>
2023-10-21 11:14:14 -0700

    weekend finished

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const PORT = 3000;
 
 const app = express();
@@ -13,11 +12,33 @@ const books = [
 
 // Define a route that accepts URL parameters 
 app.get('/books/:id', (req, res) => {
-    bookId = parseInt(req.params.id); 
+    const bookId = parseInt(req.params.id); 
     const book = books.find((b) => b.id === bookId);
-    if (!book) { return res.status(404).send("Book not found"); } 
-res.send(`Book Title: ${book.title}`); });
+    if (!book) {
+        return res.status(404).send("Book not found"); 
+    } else
+res.send(`Book Title: ${book.title}`);
+});
 
+// Handling Errors
+app.get('/fixed-example/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+    const book = books.find((b) => b.id === bookId);
+  
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    } res.json({ bookTitle: book.title });
+});
+
+// Books/Long
+app.get('/books/long', (req, res) => {
+    res.send("List of long books");
+  });
+
+// Handling Multiple Parameters: 
+app.get('greet/:firstname/:lastname', (req,res) => {
+    res.send(`Greetings ${req.params.firstname} ${req.params.lastname}`)
+})
 app.listen(PORT, () => {
     res.send(console.log(`Listening on port ${PORT}`))
 })
